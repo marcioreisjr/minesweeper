cd979888bd59c36c5d60b87af9e87c6cac11cdea
Marcio Reis Jr <marcioreisjr@gmail.com>
2023-09-03 21:57:57 -0700

    Setup loging and sign up pages to use templates. - Styled the forms using 'widgets' - Set play.html to include the JS code for the game. Other templates excudes it.

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,20 +2,41 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username',
+        })
+    )
     password = forms.CharField(
         max_length=150,
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+        })
     )
 
 
 class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=150)
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username',
+        })
+    )
     password = forms.CharField(
         max_length=150,
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+        })
     )
     password_confirmation = forms.CharField(
         max_length=150,
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm password',
+        })
     )
