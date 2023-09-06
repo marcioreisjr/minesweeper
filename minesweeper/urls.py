e0bd15270d30f56c376603005229b3d25a4e7b63
Marcio Reis Jr <marcioreisjr@gmail.com>
2023-09-06 09:16:33 -0700

    Redirected to home on startup

--- minesweeper/urls.py
+++ minesweeper/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def redirect_to_home(request):
+    return redirect("login")
+
 
 urlpatterns = [
+    path("", redirect_to_home, name='home'),
     path("admin/", admin.site.urls),
     path("coregame/", include("coregame.urls")),
     path("accounts/", include("accounts.urls")),
